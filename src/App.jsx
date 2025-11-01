304d61171aca60a921d73d07a7c6fceaf6319ae4
Nursultan Mj <nursuccessmj@gmail.com>
2025-11-01 17:13:55 +0600

    pokemons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Box, Typography } from "@mui/material";
 
 function App() {
-  return()
-}
+  const [pokemons, setPokemons] = useState([]);
 
-export default App
+  const getPokemons = async () => {
+    try {
+      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/ `);
+      console.log(data.results);
+      setPokemons(data.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <Box>
+      {pokemons.map((pokemon) => {
+        return (
+          <Box
+            display={"flex"}
+            sx={{
+              borderRadius: "10px",
+              backgroundColor: "#9edef9",
+              padding: "20px  50px ",
+              margin: "30px 30px",
+              width: "7%",
+            }}
+          >
+            <img src={pokemon.url} />
+            <Typography sx={{ marginLeft: "30px" }}>{pokemon.name}</Typography>
+          </Box>
+        );
+      })}
+    </Box>
+  );
+}
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById("root")).render(<App />);
